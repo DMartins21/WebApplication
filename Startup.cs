61a0917173a6baa858da5db208705b6cdbc9f80a
Davi <davi_martins2009@outlook.com>
2022-12-29 17:13:09 -0300

    MySQL e Migration

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApplication.Data;
 
 namespace WebApplication
 {
@@ -33,6 +30,10 @@ namespace WebApplication
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplicationContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("WebApplicationContext"), builder =>
+builder.MigrationsAssembly("WebApplication")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
