bad4e600b9f1a49711b4baafea737cb348b90e54
Davi <davi_martins2009@outlook.com>
2023-01-21 15:57:02 -0300

    Edit seller and custom service ecception

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,8 @@
 using WebApplication.Services;
 using WebApplication.Models;
 using WebApplication.Models.ViewModels;
+using WebApplication.Services.Exceptions;
+using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
@@ -72,6 +74,47 @@ namespace WebApplication.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException e)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using WebApplication.Data;
 using WebApplication.Models;
 using Microsoft.EntityFrameworkCore;
+using WebApplication.Services.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
 namespace WebApplication.Services
 {
     public class SellerService
@@ -34,6 +37,22 @@ namespace WebApplication.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
