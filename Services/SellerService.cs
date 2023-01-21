c81309621d963daef526e2c26d23b8c8ab76aa39
Davi <davi_martins2009@outlook.com>
2023-01-21 15:34:48 -0300

    Seller details and eager loading

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using WebApplication.Data;
 using WebApplication.Models;
+using Microsoft.EntityFrameworkCore;
 namespace WebApplication.Services
 {
     public class SellerService
@@ -24,7 +25,9 @@ namespace WebApplication.Services
         }
         public Seller FindById(int id) 
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller
+                .Include(obj => obj.Department)
+                .FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
