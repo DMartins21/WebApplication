9777b7c3fead53a9f15af27783e05a269a0e8187
Davi <davi_martins2009@outlook.com>
2023-01-30 15:48:39 -0300

    Asynchronus Operations using Tasks (async,await)

--- Services/DepartmentService.cs
+++ Services/DepartmentService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using WebApplication.Data;
 using WebApplication.Models;
 
@@ -14,9 +16,9 @@ namespace WebApplication.Services
                 _context = context;
             }
 
-            public List<Department> FindAll()
+            public async Task<List<Department>> FindAllAsync()
             {
-                return _context.Department.OrderBy(x => x.Name).ToList();
+                return await _context.Department.OrderBy(x => x.Name).ToListAsync();
             }
             public void Insert(Seller obj)
             {
