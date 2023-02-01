98129058f50da5a15b29d61d9700ca3df5674a07
Davi <davi_martins2009@outlook.com>
2023-02-01 15:55:19 -0300

    Implementing simple search

---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using WebApplication.Models;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using WebApplication.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WebApplication.Services
+{
+    public class SalesRecordService
+    {
+        private readonly WebApplicationContext _context;
+
+        public SalesRecordService(WebApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDataAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if(maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= minDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
